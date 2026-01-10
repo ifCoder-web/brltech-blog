7efd6cff34f53ae017942ec88e00f9a5cfa0b363
ifCoder <ifcoderweb@gmail.com>
2026-01-10 16:30:39 -0300

    ajustando DB

--- db/db.js
+++ db/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const db = async () =>{
-    await mongoose.connect(`mongodb+srv://${process.env.DB_LOGIN}:${process.env.DB_PASS}@cluster0.lpwdz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)
+    await mongoose.connect(`mongodb+srv://${process.env.DB_LOGIN}:${process.env.DB_PASS}@cluster0.jeg7xxa.mongodb.net/?appName=Cluster0`)
     .then(() => {
         console.log("DB connect!")
     }).catch((error) => {
